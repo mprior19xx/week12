6afa66028b58dfb169ec7ac2b7fa824b4841480b
Prior <m_prior85114@tss-hr420-dm14-2.local>
2018-11-22 15:36:39 -0500

    Finished men vs women

--- canadaVworld.py
+++ canadaVworld.py
@@ -1,3 +1,76 @@
 import csv 
 
-open('data/MedalData.csv')
+categories = []
+canada = []
+world = []
+
+with open('data/MedalData.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    line_count = 0
+
+    for row in reader:
+        if line_count is 0:
+        	categories.append(row)
+        	line_count += 1
+        elif row[4] == "CAN":
+        	canada.append([int(row[0]), row[5], row[6], row[7]]) # multi dimensional array 
+        else:
+        	world.append([int(row[0]), row[5], row[6], row[7]])
+        line_count += 1
+
+print('total medals for canada:', len(canada))
+print('total medals for everyone else:', len(world))
+
+print('processed', line_count, 'rows of data')
+print(canada[0])
+
+gold_1924 = []
+gold_1948 = []
+gold_1972 = []
+gold_2002 = []
+gold_2014 = []
+
+for medal in canada:
+	if medal[0] == 1924 and medal[3] == "Gold":
+		gold_1924.append(medal)
+
+	if medal[0] == 1924 and medal[3] == "Gold":
+		gold_1948.append(medal)
+
+	if medal[0] == 1924 and medal[3] == "Gold":
+		gold_1972.append(medal)
+
+	if medal[0] == 1924 and medal[3] == "Gold":
+		gold_2002.append(medal)
+
+	if medal[0] == 1924 and medal[3] == "Gold":
+		gold_2014.append(medal)
+
+
+# filter 2014 based on gender
+# plot men vs women (percentages of total)
+men = []
+women=[]
+menMedals =int(len(men)) / len(gold_2014) *100
+womenMedals = int(len(women))
+
+for gender in gold_2014:
+	if gender[1] == "Men":
+		men.append(gender)
+	elif gender[1] == 'Women':
+		women.append(gender)
+
+# chart visualization
+labels = "Men", "Women"
+sizes = [menMedals, womanMedals]
+colors = ['Blue', 'Pink']
+explode = (0.1, 0.1, 0.1)
+
+plt.pie(sizes, explode=explode, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+
+plt.legend(labels, loc=1)
+plt.title("Canadian men vs women ")
+plt.xlabel("Who won more medals")
+plt.show()
+
--- medalCount.py
+++ medalCount.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 golds = []
 silvers = []
@@ -6,7 +7,7 @@ bronzes = []
 
 categories =[]
 
-with open('data/MedalData.csv') as csvfile:
+with open('./data/MedalData.csv') as csvfile:
     reader = csv.reader(csvfile)
     line_count = 0
 
@@ -20,10 +21,10 @@ with open('data/MedalData.csv') as csvfile:
                 golds.append(row[7])
 
             elif row[7] == "Silver":
-                golds.append(row[7])
+                silvers.append(row[7])
 
             elif row[7] == "Bronze":
-                golds.append(row[7])
+                bronzes.append(row[7])
             line_count += 1
 
 print(len(golds), 'Gold medals were won since \'24')
@@ -36,18 +37,18 @@ totalMedals = len(golds) + len(silvers) + len(bronzes)
 
 gold_percentage = int(len(golds) / totalMedals * 100)
 bronze_percentage = int(len(bronzes) / totalMedals * 100)
-silver_percentage = int(len(silers) / totalMedals * 100)
+silver_percentage = int(len(silvers) / totalMedals * 100)
 
 # chart visualization
 labels = "Gold", "Silver", "Bronze"
 sizes = [gold_percentage, silver_percentage, bronze_percentage]
 colors = ['gold', 'silver', 'chocolate']
-explode = (0.1, 0.1, 0.1)
+explode = (0.2, 0.1, 0.15 )
 
-plt.pis(sizez, explode=explode, colors=colors, autopt= '%1.1f%%', shadow=true, startangle=140)
+plt.pie(sizes, explode=explode, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
 plt.axis('equal')
 
 plt.legend(labels, loc=1)
 plt.title("How many medals have been won overall?")
-plt.label("Medals won sine 1928")
+plt.xlabel("Medal Counts")
 plt.show()
